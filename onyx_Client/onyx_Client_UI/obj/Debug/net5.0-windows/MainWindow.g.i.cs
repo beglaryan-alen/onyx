8c18be562719115b2564bf279cc15885cd37f1c4
Alen Beglaryan <53264155+Wolige@users.noreply.github.com>
2021-09-23 18:15:01 +0400

    authenticatorservicesaddedcommit

--- onyx_Client/onyx_Client_UI/obj/Debug/net5.0-windows/MainWindow.g.i.cs
+++ onyx_Client/onyx_Client_UI/obj/Debug/net5.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace onyx_Client_UI {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/onyx_Client_UI;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/onyx_Client_UI;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
